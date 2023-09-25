59c168a28bf6105e2eef1a70d18ad4bb1189b5ec
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-09-24 21:32:10 -0400

    Assignment2 Created

---
+++ assignments/assignment02/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private CharacterController playerController;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _rotationSpeed = 120;
+
+
+
+    private Vector3 _movement;
+
+
+    [Header("Animations")]
+    private Animator _anim;
+    private bool _isWalking;
+    
+
+
+    private void Awake()
+    {
+        GetRefrences();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+        Destroy();
+
+    }
+
+    private void GetRefrences()
+    {
+        playerController = GetComponent<CharacterController>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Movement()
+    {
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionZ = Input.GetAxisRaw("Vertical");
+        Vector3 _movement = new Vector3(directionX, 0, directionZ);
+        _movement.Normalize();
+
+        if (_movement == Vector3.zero)
+        {
+            _anim.SetBool("_isWalking", false);
+            
+        }
+        else
+        {
+            _anim.SetBool("_isWalking", true);
+            
+            Quaternion toRotation = Quaternion.LookRotation(_movement, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, _rotationSpeed * Time.deltaTime);
+        }
+
+        playerController.Move(_movement * _speed * Time.deltaTime);
+
+
+    }
+
+    private void Destroy()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _anim.SetTrigger("Attack");
+        }
+
+    }
+
+
+}
